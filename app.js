2856f3b470d377266f32872515ce927a6dbf7d4e
Andres Nodas <nodasiniesta@gmail.com>
2021-01-17 23:25:31 +0700

    Establishing a Connection From the Client

--- app.js
+++ app.js
@@ -68,7 +68,12 @@ mongoose
         console.log('Connected!');
         const server = app.listen(8080);
 
-        const io = require('socket.io')(server);
+        const io = require('socket.io')(server, {
+            cors: {
+                origin: 'http://localhost:3000',
+                methods: ['GET', 'POST'],
+            },
+        });
         io.on('connection', (socket) => {
             console.log('Client connected.');
         });
